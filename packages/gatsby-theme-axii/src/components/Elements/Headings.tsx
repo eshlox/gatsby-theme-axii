5cd2c4a3a78d6a83a12d98722aba9cdab2ec8500
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2019-11-03 21:39:35 +0100

    feat(component): headings component

---
+++ packages/gatsby-theme-axii/src/components/Elements/Headings.tsx
@@ -0,0 +1,26 @@
+import { Heading } from "@chakra-ui/core";
+import React from "react";
+
+export const h1: React.FC = props => {
+  return <Heading as="h1" fontSize="5xl" my="1em" {...props}></Heading>;
+};
+
+export const h2: React.FC = props => {
+  return <Heading as="h2" fontSize="3xl" my="1em" {...props}></Heading>;
+};
+
+export const h3: React.FC = props => {
+  return <Heading as="h3" fontSize="xl" my="1em" {...props}></Heading>;
+};
+
+export const h4: React.FC = props => {
+  return <Heading as="h4" fontSize="lg" my="1em" {...props}></Heading>;
+};
+
+export const h5: React.FC = props => {
+  return <Heading as="h5" fontSize="md" my="1em" {...props}></Heading>;
+};
+
+export const h6: React.FC = props => {
+  return <Heading as="h6" fontSize="sm" my="1em" {...props}></Heading>;
+};
