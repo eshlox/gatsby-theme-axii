90b60d6baabc2a8737bd1d9afac02b26da94677c
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2019-11-09 15:41:43 +0100

    feat(gatsby-config): example configuration

---
+++ packages/site/gatsby-config.js
@@ -0,0 +1,57 @@
+const siteMetadata = {
+  siteUrl: `https://gatsby-theme-axii.netlify.com`,
+  siteTitle: `AXII - A Gatsby theme`,
+  siteDescription: `Very simple blog theme.`,
+  author: {
+    nickname: `eshlox`,
+    name: `Przemysław Kołodziejczyk`,
+    email: `eshlox@vertolabs.com`
+  },
+  social: {
+    twitter: {
+      username: "eshlox",
+      url: "https://twitter.com/eshlox"
+    },
+    github: {
+      url: "https://github.com/eshlox/gatsby-theme-axii"
+    },
+    linkedin: {
+      url: "https://linkedin.com/eshlox"
+    },
+    telegram: {
+      url: "https://telegram.me/eshlox"
+    }
+  },
+  support: {
+    buymeacoffee: {
+      url: "https://www.buymeacoffee.com/eshlox"
+    }
+  },
+  comments: {
+    disqus: {
+      shortname: "axii-a-gatsby-theme"
+    }
+  },
+  search: {
+    algolia: {
+      posts: {
+        applicationId: process.env.ALGOLIA_APPLICATION_ID,
+        apiKey: process.env.ALGOLIA_API_KEY
+      }
+    }
+  }
+};
+
+const plugins = [
+  {
+    resolve: "@eshlox/gatsby-theme-axii",
+    options: {
+      contentPosts: "content/posts"
+    }
+  }
+];
+
+module.exports = {
+  plugins,
+  siteMetadata
+};
