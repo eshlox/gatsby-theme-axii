b2c680c2ac3c2251a0e12c49ffe9d07191593bd4
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2019-11-09 15:27:09 +0100

    improvement(components): mdxcomponents

---
+++ packages/gatsby-theme-axii/src/components/MDXComponents.tsx
@@ -0,0 +1,45 @@
+import {
+  blockquote,
+  code,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  hr,
+  inlineCode,
+  li,
+  ol,
+  p,
+  pre,
+  table,
+  td,
+  th,
+  ul,
+  youtube
+} from "./Elements";
+
+const MDXComponents = {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  hr,
+  inlineCode,
+  code,
+  blockquote,
+  youtube,
+  pre,
+  table,
+  th,
+  td,
+  ul,
+  ol,
+  li
+};
+
+export default MDXComponents;
