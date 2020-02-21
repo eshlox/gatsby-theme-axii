57b42862956e7d9784a106b9dd4552dc1d225ebc
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2020-02-21 12:52:44 +0100

    feat(pre): preformatted text component

--- packages/gatsby-theme-axii/src/components/Elements/Pre/component.tsx
+++ packages/gatsby-theme-axii/src/components/Elements/Pre/component.tsx
@@ -1,5 +1,16 @@
+import Container from "@material-ui/core/Container";
 import React from "react";
 
-const pre: React.FC = props => <pre {...props} />;
+const pre: React.FC<{ children: JSX.Element }> = props => {
+  if (props.children?.props?.originalType === "code") {
+    return props.children;
+  }
+
+  return (
+    <Container maxWidth="md">
+      <pre {...props} />
+    </Container>
+  );
+};
 
 export default pre;
