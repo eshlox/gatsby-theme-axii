fdeb7b336c0b72c93cceb788ae74fc77c59904f7
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2020-03-30 22:10:41 +0200

    refactor(fonts): update fonts and headers

--- packages/gatsby-theme-axii/src/components/Elements/Headings/styles.ts
+++ packages/gatsby-theme-axii/src/components/Elements/Headings/styles.ts
@@ -5,32 +5,32 @@ const useStyles = makeStyles((theme: Theme) =>
     h1: {
       ...theme.typography.h1,
       marginBottom: theme.spacing(2),
-      marginTop: theme.spacing(4)
+      marginTop: theme.spacing(8)
     },
     h2: {
       ...theme.typography.h2,
       marginBottom: theme.spacing(2),
-      marginTop: theme.spacing(4)
+      marginTop: theme.spacing(8)
     },
     h3: {
       ...theme.typography.h3,
       marginBottom: theme.spacing(2),
-      marginTop: theme.spacing(4)
+      marginTop: theme.spacing(8)
     },
     h4: {
       ...theme.typography.h4,
       marginBottom: theme.spacing(2),
-      marginTop: theme.spacing(4)
+      marginTop: theme.spacing(8)
     },
     h5: {
       ...theme.typography.h5,
       marginBottom: theme.spacing(2),
-      marginTop: theme.spacing(4)
+      marginTop: theme.spacing(8)
     },
     h6: {
       ...theme.typography.h6,
       marginBottom: theme.spacing(2),
-      marginTop: theme.spacing(4)
+      marginTop: theme.spacing(8)
     }
   })
 );
--- packages/gatsby-theme-axii/src/theme.ts
+++ packages/gatsby-theme-axii/src/theme.ts
@@ -7,39 +7,12 @@ const baseTheme: ThemeOptions = {
   typography: {
     htmlFontSize: 18,
     fontSize: 18,
-    fontFamily: [
-      "system-ui",
-      "-apple-system",
-      "BlinkMacSystemFont",
-      "Segoe UI",
-      "Roboto",
-      "Helvetica Neue",
-      "Arial",
-      "Noto Sans",
-      "sans-serif",
-      "Apple Color Emoji",
-      "Segoe UI Emoji",
-      "Segoe UI Symbol",
-      "Noto Color Emoji"
-    ].join(","),
-    h1: {
-      fontSize: "4rem"
-    },
-    h2: {
-      fontSize: "3rem"
-    },
-    h3: {
-      fontSize: "2.25rem"
-    },
-    h4: {
-      fontSize: "1.875rem"
-    },
-    h5: {
-      fontSize: "1.5rem"
-    },
-    h6: {
-      fontSize: "1.25rem"
-    }
+    h1: { fontSize: "4rem" },
+    h2: { fontSize: "3rem" },
+    h3: { fontSize: "2.25rem" },
+    h4: { fontSize: "1.875rem" },
+    h5: { fontSize: "1.5rem" },
+    h6: { fontSize: "1.25rem" }
   },
   palette: {
     primary: blue,
@@ -50,11 +23,7 @@ const baseTheme: ThemeOptions = {
 let darkThemeOptions = { ...baseTheme };
 darkThemeOptions!.palette!.type = "dark";
 
-const darkTheme = responsiveFontSizes(
-  createMuiTheme({
-    ...darkThemeOptions
-  })
-);
+const darkTheme = responsiveFontSizes(createMuiTheme({ ...darkThemeOptions }));
 
 let lightThemeOptions = { ...baseTheme };
 lightThemeOptions!.palette!.type = "light";
