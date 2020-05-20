7a8085b8cafcf5fd565ceb0e3a3a81c02f6c94ec
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2020-05-20 23:28:13 +0200

    fix(blog): article list as mdx page

---
+++ packages/gatsby-theme-axii/src/components/ArticleList/index.tsx
@@ -0,0 +1,40 @@
+import List from "@material-ui/core/List";
+import { graphql, useStaticQuery } from "gatsby";
+import React from "react";
+import ArticleListItem from "../shared/ArticleListItem";
+import useAllArticlesStyles from "./styles";
+
+const query = graphql`
+  {
+    allArticle {
+      edges {
+        node {
+          objectID: id
+          slug
+          title
+          date(formatString: "YYYY-MM-DD")
+        }
+      }
+    }
+  }
+`;
+
+const ArticleListPage: React.FC = () => {
+  const classes = useAllArticlesStyles();
+  const articles = useStaticQuery(query);
+
+  return (
+    <List className={classes.list}>
+      {articles.allArticle.edges.map((edge: any) => (
+        <ArticleListItem
+          key={edge.node.objectID}
+          slug={edge.node.slug}
+          title={edge.node.title}
+          date={edge.node.date}
+        />
+      ))}
+    </List>
+  );
+};
+
+export default ArticleListPage;
